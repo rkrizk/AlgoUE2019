4d16cc294eabbe525336c2934fa998de64c45a28
rkriz <richard.kriz@hotmail.com>
2019-04-04 18:35:02 +0200

    add option for diagonal path

---
+++ A4/rkrizk-ManhattenTouristHVD.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+import sys
+
+matrix_list = []
+row_list = []
+row = " "
+# read STDIN and convert to nested list
+for line in sys.stdin:
+    if line.startswith("G"):
+        continue
+    elif line == "---\n":
+        matrix_list.append(row_list)
+        row_list = []
+    else:
+        for char in line:
+            if char == " " and row[-1] == " ":
+                continue
+            if char == "\n":
+                row = row.strip("\n")
+                row = row.strip(" ")
+                row_list.append(row.split(" "))
+                row = " "
+            else:
+                row += char
+
+G_down = matrix_list[0]
+G_right = matrix_list[1]
+G_diag = matrix_list[2]
+edge_length = len(G_down[0])
+
+# check input matrices
+for i in range(edge_length-1):
+    if len(G_down[i]) != edge_length:
+        print("G_down matrix corrupt?")
+
+for i in range(edge_length):
+    if len(G_right[i]) != edge_length-1:
+        print("G_right matrix corrupt?")
+
+for i in range(edge_length-1):
+    if len(G_diag[i]) != edge_length-1:
+        print("G_diag matrix corrupt?")
+
+matrix = [[0]]
+# initialize first row and column of matrix
+for i in range(edge_length-1):
+    matrix[0].append(round(matrix[0][i]+float(G_right[0][i]), 2))
+
+for i in range(edge_length-1):
+    matrix.append([round(matrix[i][0]+float(G_down[i][0]), 2)])
+
+# fill matrix horizontally
+for row in range(1, edge_length):
+    for col in range(1, edge_length):
+        matrix[row].append(round(max(matrix[row-1][col]+float(G_down[row-1][col]),
+                                     matrix[row][col-1]+float(G_right[row][col-1]),
+                                     matrix[row-1][col-1]+float(G_diag[row-1][col-1])), 2))
+
+# for i in range(edge_length):
+#     print(matrix[i])
+
+print(matrix[-1][-1])
